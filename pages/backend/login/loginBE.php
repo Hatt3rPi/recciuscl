cada287d4731fc673f6e3de643bcbce2be273e19
Hatt3rPi <fabarca212@gmail.com>
2024-03-26 19:40:40 +0000

    Actualizar el repositorio productivo

--- pages/backend/login/loginBE.php
+++ pages/backend/login/loginBE.php
@@ -1,7 +1,7 @@
 <?php
 // pages\backend\login\loginBE.php
 session_start();
-require_once "/home/customw2/conexiones/config_reccius.php";
+require_once "/home/recciusc/conexiones/config_reccius.php";
 
 function escape($value) {
     global $link;
