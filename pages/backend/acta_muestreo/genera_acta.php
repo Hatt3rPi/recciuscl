d99f72359c95fad92bce49787ae3f91c642e6c8d
Hatt3rPi <fabarca212@gmail.com>
2025-09-16 13:55:45 +0000

    Actualizar el repositorio productivo

--- pages/backend/acta_muestreo/genera_acta.php
+++ pages/backend/acta_muestreo/genera_acta.php
@@ -1,9 +1,9 @@
 <?php
 //archivo pages\backend\acta_muestreo\genera_acta.php
 session_start();
-require_once "/home/customw2/conexiones/config_reccius.php";
+require_once "/home/recciusc/conexiones/config_reccius.php";
 if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
-    header("Location: https://customware.cl/reccius/pages/login.html");
+    header("Location: https://reccius.cl/customware/pages/login.html");
     exit;
 }
 $tipo_producto='';
--- pages/backend/cloud/R2_manager.php
+++ pages/backend/cloud/R2_manager.php
@@ -1,15 +1,15 @@
 <?php
 // pages\backend\cloud\R2_manager.php
 session_start();
-require_once "/home/customw2/conexiones/config_reccius.php";
-require_once "/home/customw2/librerias/aws/aws-autoloader.php";
+require_once "/home/recciusc/conexiones/config_reccius.php";
+require_once "/home/recciusc/librerias/aws/aws-autoloader.php";
 //require_once "../../../assets/dependencies/aws/aws-autoloader.php";
 
 use  Aws\Credentials\Credentials;
 use Aws\S3\S3Client;
 
 if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
-  header("Location: https://customware.cl/reccius/pages/login.html");
+  header("Location: https://reccius.cl/customware/pages/login.html");
   exit;
 }
 
